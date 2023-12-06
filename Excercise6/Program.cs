d2664c91000ca2002003436045c42acfdaab834f
Memby Poudel <officialmembypoudel@gmail.com>
2023-12-06 21:49:15 +0545

    First Commit

---
+++ Excercise6/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Excercise6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running, Program!");
+
+        Stack<int> stack = new Stack<int>();
+
+        static void PrintStack(Stack <int> stack)
+        {
+            Console.WriteLine("The data of stack:");
+            for (int i = 0; i < stack.data.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}: {stack.data[i]}");
+            }
+        }
+
+        Console.WriteLine($"The stack is empty: {stack.IsEmpty()}");
+
+        stack.Push(10);
+        PrintStack(stack);
+
+        stack.Push(1);
+        PrintStack(stack);
+
+        stack.Push(15);
+        PrintStack(stack);
+
+        Console.WriteLine($"The stack is empty: {stack.IsEmpty()}");
+
+        int popValue = stack.Pop();
+        PrintStack(stack);
+        Console.WriteLine($"The popped value is: {popValue}.");
+
+
+        stack.Push(12);
+        PrintStack(stack);
+
+        stack.Push(13);
+        PrintStack(stack);
+
+        popValue = stack.Pop();
+        PrintStack(stack);
+        Console.WriteLine($"The popped value is: {popValue}.");
+
+        Console.WriteLine($"The stack is empty: {stack.IsEmpty()}");
+
+        int peekValue = stack.Peek();
+        Console.WriteLine($"The peeked value is: {peekValue}.");
+
+        Console.ReadKey();
+    }
+}
+
---
+++ Excercise6/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Excercise6
+{
+	public class Stack<T>
+	{
+		public List<T> data;
+
+        public Stack()
+        {
+            this.data = new List<T>();
+        }
+
+		public void Push(T value)
+		{
+			data.Add(value);
+		}
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("The list is empty.");
+            }
+
+            int lastIndex = data.Count - 1;
+
+            T value = data[lastIndex];
+            data.RemoveAt(data.Count-1);
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("The list is empty.");
+
+            }
+            return data[data.Count -1];
+        }
+
+        public bool IsEmpty()
+        {
+            if(data.Count != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
